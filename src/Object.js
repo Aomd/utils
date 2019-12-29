b3f2299efca300bc4d11ddcec571c6372142c940
Aomd <g00665@163.com>
2019-12-29 21:44:15 +0800

    添加取值对象

--- src/Object.js
+++ src/Object.js
@@ -9,3 +9,30 @@ export const size = (o) => {
 export const pluck = () => {
 
 }
+
+export const getDefined = (pototype, defined) =>{
+    if (typeof pototype !== 'string') {
+      return defined;
+    }
+    var pototypeArray = pototype.split('.');
+    var baseItem = ''
+    try {
+      if (/[a-z|A-Z|0-9|_]+/.test(pototypeArray[0])) {
+        baseItem = eval(pototypeArray[0]);
+      }else{
+        return defined;
+      }
+
+      for (var i = 1; i < pototypeArray.length; i++) {
+        if (pototypeArray[i] in baseItem) {
+          baseItem = baseItem[pototypeArray[i]]
+        } else {
+          return defined;
+        }
+      }
+      return baseItem
+    } catch (error) {
+      return defined
+    }
+
+  }

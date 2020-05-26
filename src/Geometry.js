de94a501e6a8cc9f1d5c19784dab387ad824c242
Aomd <g00665@163.com>
2020-05-26 20:16:57 +0800

    添加 通过固定值取点  折线等分

--- src/Geometry.js
+++ src/Geometry.js
@@ -8,7 +8,7 @@ function averageLine(point1, point2, num) {
     var x1 = point1[0];
     var y1 = point1[1];
     var x2 = point2[0];
-    var x2 = point2[1];
+    var y2 = point2[1];
 
     var o = [point1];
     for (var i = 1; i < num; i++) {
@@ -19,6 +19,84 @@ function averageLine(point1, point2, num) {
     return o;
 }
 
-export{
-    averageLine
+/**
+ * 取线段多少米的点
+ *
+ * @param {*} point1 起始点 
+ * @param {*} point2 终止点
+ * @param {*} m 值 单位m
+ * @returns
+ */
+function getPointFromLine(point1, point2, m) {
+    // 距离为零就直接返回 point1
+    if (+m === 0) {
+        return point1;
+    }
+    var x1 = point1[0];
+    var y1 = point1[1];
+    var x2 = point2[0];
+    var y2 = point2[1];
+    // AMap.GeometryUtil  https://lbs.amap.com/api/javascript-api/reference/math 
+    // AMap.GeometryUtil.distance 两点求距离 m
+    var temp_m = AMap.GeometryUtil.distance(point1, point2)
+    var num = temp_m / m
+    var x = (x2 + (num - 1) * x1) / num;
+    var y = (y2 + (num - 1) * y1) / num;
+
+    return [x, y]
+}
+
+/**
+ *
+ *
+ * @param {*} points 点集合
+ * @param {*} val 值 单位m
+ * @returns
+ */
+function averagePolyline(points, val) {
+    // 所有点
+    var pointAll = [points[0]];
+    // 当前剩余量
+    var surplus = 0;
+    // 按线段长
+    for (var i = 0; i < points.length - 1; i++) {
+        // 总长
+        // AMap.GeometryUtil  https://lbs.amap.com/api/javascript-api/reference/math 
+        // AMap.GeometryUtil.distance 两点求距离 m
+        var total_distance = AMap.GeometryUtil.distance(points[i], points[i + 1])
+
+        // 如果第一段余量+第二段总长还不够 val 就跳过直接加上余量
+        if (total_distance + surplus < val && surplus != 0) {
+            surplus += total_distance
+            continue;
+        }
+
+        // 总段数
+        var total_section = Math.floor(total_distance / val);
+
+        // 头点计算
+        var new_point_start = getPointFromLine(points[i], points[i + 1], val - surplus);
+        // 末点计算
+        var new_point_ends = getPointFromLine(new_point_start, points[i + 1], total_section * val)
+
+        var temp_val = averageLine(new_point_start, new_point_ends, total_section)
+
+        // 合并结果
+        pointAll = pointAll.concat(temp_val);
+
+        if (surplus === val) {
+            // 计算剩余量
+            surplus = total_distance % val;
+        } else {
+            // 计算剩余量
+            surplus = (total_distance + surplus) % val;
+        }
+    }
+    return pointAll;
+}
+
+export {
+    averageLine,
+    getPointFromLine,
+    averagePolyline
 }
